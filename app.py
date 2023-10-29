5cce74db71ed85c5722c31faa982735068481360
Alex Delicer <adelicer24@wooster.edu>
2023-10-28 23:18:52 -0400

    all stat pages done

--- app.py
+++ app.py
@@ -22,13 +22,45 @@ def comparison():
 def glossary():
     return render_template("base-templates/glossary.html")
 
-@app.route("/statistics-teams")
-def teams():
-    return render_template("stat-templates/teams.html")
-
 @app.route("/statistics-awards")
 def awards():
     return render_template("stat-templates/awards.html")
+
+@app.route("/statistics-battingstd")
+def battingstd():
+    return render_template("stat-templates/batting-std.html")
+
+@app.route("/statistics-battingadv")
+def battingadv():
+    return render_template("stat-templates/batting-adv.html")
+
+@app.route("/statistics-battingsab")
+def battingsab():
+    return render_template("stat-templates/batting-sab.html")
+
+@app.route("/statistics-fieldingstd")
+def fieldingstd():
+    return render_template("stat-templates/fielding-std.html")
+
+@app.route("/statistics-fieldingadv")
+def fieldingadv():
+    return render_template("stat-templates/fielding-adv.html")
+
+@app.route("/statistics-pitchingstd")
+def pitchingstd():
+    return render_template("stat-templates/pitching-std.html")
+
+@app.route("/statistics-pitchingadv")
+def pitchingadv():
+    return render_template("stat-templates/pitching-adv.html")
+
+@app.route("/statistics-pitchingsab")
+def pitchingsab():
+    return render_template("stat-templates/pitching-sab.html")
+
+@app.route("/statistics-teams")
+def teams():
+    return render_template("stat-templates/teams.html")
 
 #Next job: Fill in statistics.html with info of what stats are displayed under each category
 #Then create a single batting, fielding, and pitching page
--- csv-to-html.py
+++ csv-to-html.py
@@ -1,7 +1,7 @@
 from prettytable import PrettyTable
 
 # open csv file
-a = open("mlb_data/Awards_2018-2023.csv", 'r')
+a = open("mlb_data/Fielding_Advanced_2018-2023.csv", 'r')
  
 # read the csv file
 a = a.readlines()
@@ -11,12 +11,12 @@ l1 = a[0]
 l1 = l1.split(',')
  
 # headers for table
-t = PrettyTable([l1[0],l1[1],l1[2],l1[3],l1[4],l1[5],l1[6],l1[7],l1[8],l1[9],l1[10],l1[11],l1[12],l1[13],l1[14],l1[15],l1[16],l1[17],l1[18],l1[19]])
+t = PrettyTable([l1[0],l1[1],l1[2],l1[3],l1[4],l1[5],l1[6],l1[7],l1[8],l1[9],l1[10]])
  
 # Adding the data
 for i in range(1, len(a)) :
     t.add_row(a[i].split(','))
  
 code = t.get_html_string()
-html_file = open('awards.html', 'w')
+html_file = open('fielding2.html', 'w')
 html_file = html_file.write(code)
