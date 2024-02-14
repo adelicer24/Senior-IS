b45f2821b4c86d5e25d34231553bc0da245ca6bd
Alex Delicer <adelicer24@wooster.edu>
2024-02-14 16:56:43 -0500

    more minor fixes

--- csv-to-html.py
+++ csv-to-html.py
@@ -12,7 +12,7 @@ l1 = a[0]
 l1 = l1.split(',')
  
 # Choose the headers to include from table
-t = PrettyTable([l1[0],l1[1],l1[2],l1[3],l1[4],l1[5],l1[6],l1[7],l1[8],l1[9],l1[10]])
+t = PrettyTable([l1[0],l1[1],l1[2],l1[3],l1[4],l1[5]])
  
 # Adding the table data
 for i in range(1, len(a)) :
@@ -22,7 +22,7 @@ for i in range(1, len(a)) :
 code = t.get_html_string()
 
 # Open a new HTML file to write into
-html_file = open('.html', 'w')
+html_file = open('my_table.html', 'w')
 
 # Write the table into the html file
 html_file = html_file.write(code)
--- html-to-csv.py
+++ html-to-csv.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Read all HTML tables from given URL
-tables = pd.read_html('https://www.baseball-reference.com/leagues/majors/2023-standard-batting.shtml')
+tables = pd.read_html('https://www.baseball-reference.com/leagues/majors/2023-standings.shtml#expanded_standings_overall')
 
 # Write HTML table to a CSV file
 tables[0].to_csv('my_file.csv')
